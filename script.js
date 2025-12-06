03833ca3f002290c19d937ce3be5c12668e489c9
Foley <teriomon5@aol.com>
2025-12-06 18:08:59 -0500

    pre-refactoring getRandomColor

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+let rowInput = 16;
+let columnInput = 16;
+let lastRowInput = 0;
+let lastColumnInput = 0;
+let darkenToggle = false;
+let darkenAmount = 0;
+let hoverColor = "random"
 const MAXGRID = 800;
 document.documentElement.style.setProperty("--container-grid-size", `${MAXGRID}px`);
 //Create a function called random color that when called will return a number from 0-255.
-function giveRandomColor() {
-    return num = Math.floor(Math.random() * 255);
+function getRandomColor() {
+    const hue = Math.floor(Math.random() * 360);
+    const saturation = Math.floor(Math.random() * 100);
+    let lightness = Math.floor(Math.random() * 100);
+    if (darkenToggle) {
+        lightness = lightness - lightness*darkenAmount;
+        darkenAmount += 0.1;
+        console.log(darkenAmount)
+    }
+    if (darkenAmount >= 1) {
+        darkenAmount = 0;
+    }
+    return `hsl(${hue},${saturation}%, ${lightness}%)`;
 }
 //Optional a second function that does the same thing but subtracts 10% * numberOfHovers.  (have numberOfHovers reset every time 10 squares.)
 
@@ -20,7 +38,7 @@ function createGrid(x, y) {
         div.classList.add("grid-div");
         //The event listener will be waiting for mouseenter.  Upon doing so it will set the background color of a div to rgb() with all three numbers being decided by randomColor
         div.addEventListener("mouseenter", (event) =>{
-            event.target.style.backgroundColor = `rgb(${giveRandomColor()},${giveRandomColor()}, ${giveRandomColor()})`;
+            event.target.style.backgroundColor = getRandomColor();
         })
         container.appendChild(div);
     }
@@ -34,8 +52,8 @@ createGrid(16, 16);
 const newGridBtn = document.querySelector("#resize");
 newGridBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    let rowInput = Math.floor(document.querySelector("#row-input").value);
-    let columnInput = Math.floor(document.querySelector("#column-input").value);
+    rowInput = Math.floor(document.querySelector("#row-input").value);
+    columnInput = Math.floor(document.querySelector("#column-input").value);
     if (rowInput > 100) {
         rowInput = 100;
     }
@@ -44,5 +62,76 @@ newGridBtn.addEventListener("click", (event) => {
     }
     if (rowInput && columnInput) {
         createGrid(rowInput,columnInput);
+        lastRowInput = rowInput;
+        lastColumnInput = columnInput;
+    }
+    resizeBox.classList.remove("open");
+    cover.classList.remove("open");
+})
+
+
+document.querySelector(".color-selectors").addEventListener("click", (event) => {
+    const ID = event.target.id
+    console.log(ID)
+    console.log(event.target)
+    console.log(event.target.id)
+    switch (ID) {
+        case "darken-toggle":
+            darkenToggle = !darkenToggle;
+            break;
+        case "red":
+            hoverColor = "red";
+            break;
+        case "blue":
+            hoverColor = "blue";
+            break;
+        case "green":
+            hoverColor = "green";
+            break;
+        case "purple":
+            hoverColor = "purple";
+            break;
+        case "yellow":
+            hoverColor = "yellow";
+            break;
+        case "white":
+            hoverColor = "white";
+            break;
+        case "black":
+            hoverColor = "black";
+            break;
+        case "random":
+            hoverColor = "random";
+            break;
+    }
+    
+})
+//The resize button open and close.
+const resizeBox = document.querySelector(".resize-box");
+const cover = document.querySelector(".cover");
+cover.addEventListener("click", () => {
+    resizeBox.classList.remove("open");
+    cover.classList.remove("open");
+})
+document.querySelector("#modal-toggle").addEventListener("click", () => {
+    resizeBox.classList.toggle("open");
+    cover.classList.toggle("open");
+})
+
+
+document.querySelector("#reset").addEventListener("click", () => {
+    if (lastColumnInput && lastRowInput) {
+        createGrid(lastRowInput,lastColumnInput);
+    } else {
+        createGrid(rowInput, columnInput)
     }
 })
+
+function getRandomBackgroundColor() {
+    const hue = Math.floor(Math.random() * 360);
+    const saturation = Math.floor(Math.random() * 100);
+    const lightness = Math.floor(Math.random() * 100);
+    document.documentElement.style.setProperty("--random-background-color",`hsl(${hue},${saturation}%, ${lightness}%`);
+    document.documentElement.style.setProperty("--random-frontground-color",`hsl(${hue},${saturation}%, ${(lightness + 50) % 100}%`);
+}
+setInterval(getRandomBackgroundColor,1500);
